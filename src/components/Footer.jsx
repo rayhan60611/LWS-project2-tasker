f82a11c5f6f77a05cc95df0be6b2d25d5fb237a1
rayhan60611 <pecon.alok@gmail.com>
2024-10-15 10:54:01 +0200

    hero section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="py-6 md:py-8">
+    <footer className="py-6 md:py-8 !bg-[#191D26]">
       <div className="container mx-auto">
-        <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+        <p className="text-center text-xs text-white lg:text-sm">
+          Copyright ©2024 | All rights reserved by M M Rayhan Parvez
         </p>
       </div>
     </footer>
