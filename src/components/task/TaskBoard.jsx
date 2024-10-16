9864903639e8b4ddfb26826b4a03cb47d34f5127
rayhan60611 <pecon.alok@gmail.com>
2024-10-16 08:33:38 +0200

    default data load and pased as aprops into tasklist

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,10 +1,22 @@
 // import React from 'react';
 
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description: "I want to learn react so that i shine in my future goals",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavourite: true,
+  };
+
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section
       className="mb-20 flex flex-col justify-center items-center md:px-6"
@@ -18,7 +30,7 @@ const TaskBoard = () => {
         {/* <!-- Search Box Ends --> */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
